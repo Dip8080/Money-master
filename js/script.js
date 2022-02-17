65a1e53276a33ec6c549a312b109148a070f950b
dip8080 <dipmollik7@gmail.com>
2022-02-17 14:30:47 +0600

    5th commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 function AllMighty(InputId){
     if(InputId=='income_input'){
     const incomeField = document.getElementById(InputId).value;
-    console.log(typeof(incomeField))
-    console.log(incomeField)
     return incomeField ;
     }
     else if(InputId=='save_field'){
@@ -23,9 +21,20 @@ function AllMighty(InputId){
 function setBalance(totEx , totblnc){
 const totalExpfield = document.getElementById('total_expence');
 const totalblncfield = document.getElementById('total_balance');
-totalExpfield.innerText =totEx ;
-totalblncfield.innerText = totblnc ;
+if (isNaN(totEx) || isNaN(totblnc)){
+    alert('i m begging , please enter Number not string')
+    totEx = 0;
+    totblnc=0 ;
+    totalExpfield.innerText =totEx ;
+    totalblncfield.innerText = totblnc ;
 }
+else{
+    totalExpfield.innerText =totEx;
+    totalblncfield.innerText = totblnc ;
+}
+}
+
+
 // Error management
 const input_field_all= document.getElementsByClassName('block');
 for(let classx of input_field_all ){
@@ -34,9 +43,13 @@ for(let classx of input_field_all ){
     })
     classx.addEventListener('keyup',function(event){
         const typedKey = event.target.value;
+        const alphabet = 'abcdefghijklmnopqrstuvwxyz';
         if(typedKey=="-"){
             alert('click "OK" .then enter positive number');
             event.target.style.backgroundColor ="#C1C1C1"
+        }
+        else if(alphabet.indexOf(typedKey)!=-1){
+            alert('Type only NUMBER key Plese');
         }
         else{
             event.target.style.backgroundColor ="white"
@@ -47,11 +60,14 @@ for(let classx of input_field_all ){
 
 // event one
 document.getElementById('calculate_btn').addEventListener('click',function (){
-  const income = parseInt(AllMighty('income_input'));
-  console.log(typeof(income));
-  const totalExpenses = parseInt(AllMighty());
-  const balance = income - totalExpenses ;
-  setBalance(totalExpenses,balance);
+    const income = parseInt(AllMighty('income_input'));
+    const totalExpenses = parseInt(AllMighty());
+  
+    const balance = income - totalExpenses ;
+    console.log(totalExpenses);
+    console.log(balance);
+    setBalance(totalExpenses,balance); 
+  
 })
 // event two
 document.getElementById('save_btn').addEventListener('click',function(){
